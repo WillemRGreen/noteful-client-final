eccaabe58a5f14d479a8a5b64cdb34fac4a2241e
Willem Green <willberforce15@gmail.com>
2020-11-16 10:21:35 -0600

    prod update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export default {API_ENDPOINT: 'http://localhost:8000'}
-  //API_ENDPOINT: 'https://mighty-taiga-35688.herokuapp.com',
-
+export default {
+  API_ENDPOINT: 'https://mighty-taiga-35688.herokuapp.com',
+}
