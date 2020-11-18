c808e44b26ee62cc8b83f4833627e1ed2910547c
Willem Green <willberforce15@gmail.com>
2020-11-18 13:33:46 -0600

    fixed delete bugs

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://mighty-taiga-35688.herokuapp.com',
+  //API_ENDPOINT: 'https://mighty-taiga-35688.herokuapp.com',
+  API_ENDPOINT: 'http://localhost:8000'
 }
